ce6e46b06d8a0e8723e147c9e6e83aa8707c8c43
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-19 03:40:21 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+// Author: Shakeb Siddiqui sms8508@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int digit_sum(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  else {
+    return n % 10 +digit_sum(n / 10);
+  }
+}
+
+int main(void){
+  int n = atof(readline("Enter an int: "));
+  printf("sum of digits %d is %d.\n", n , digit_sum(n));
+}
