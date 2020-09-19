edcf59423caffbea12cee54c6a869290567be525
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-19 03:52:28 +0000

    of

--- main.c
+++ main.c
@@ -14,5 +14,5 @@ int digit_sum(int n) {
 }
 int main(void){
   int n = atof(readline("Enter an int: "));
-  printf("sum of digits %d is %d.\n", n , digit_sum(n));
+  printf("sum of digits of %d is %d.\n", n , digit_sum(n));
 }
